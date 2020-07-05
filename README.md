49d5af997b2099eb22da9259bcecf81d58c8164c
Cleber Miranda Barbosa <cleber@gmail.com>
2020-07-05 19:31:35 -0400

    finished exercise

--- README.md
+++ README.md
@@ -51,3 +51,4 @@ And return the updated list of todos
 
 It's going to remove one todo based on a given position at the end of the url, and return the updated list of todos.
 
+##  
---
+++ src/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, jsonify, request, json
+app = Flask(__name__)
+
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    # suppose you have some information in a json variable
+    some_data = todos
+
+    # you can convert that variable into a json string like this
+    json_text = jsonify(some_data)
+
+    # and then you can return it on the response body like this
+    return json_text
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.data
+    print('Incoming request with the following body', request_body)
+
+    decoded_object = json.loads(request_body )
+    todos.append(decoded_object)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print('This is the position to delete: ', position)
+
+    todos.pop(position)
+    return jsonify(todos)
+
+
+
+# These two lines should always be at the end of your app.py file.
+if __name__ == '__main__':
+  app.run(port=3246, debug=True)
